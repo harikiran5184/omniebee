1f5879fc234159f2e00aa0f940f9ceaa6c9dfa33
hari kiran <vhkhari2017@gmail.com>
2026-08-16 06:31:10 +0530

    deploy checking phase

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     domains: ["res.cloudinary.com"],
+    unoptimized: true,
   },
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/omniebee-global-solutions"
+      : "",
+
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/omniebee-global-solutions/"
+      : "",
 };
 
 export default nextConfig;
